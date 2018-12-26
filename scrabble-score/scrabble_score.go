df6b172f651b1cf3f8ab46969be3bdb8c7fecb96
Renato Suero <suerorenato@gmail.com>
2018-12-26 18:37:16 +0100

    Fixed golint and go fmt

--- scrabble-score/scrabble_score.go
+++ scrabble-score/scrabble_score.go
@@ -4,35 +4,35 @@ import (
 	"strings"
 )
 
-
+//Score  compute the scrabble score for the word
 func Score(text string) int {
 	text = strings.ToLower(text)
-	result :=0
+	result := 0
 	for _, key := range text {
 		letter := string(key)
-		result+= defineValue(letter)
+		result += defineValue(letter)
 	}
 	return result
 }
 
-func defineValue(letter string) int{
-		if letter == "q" || letter == "z" {
-			return 10
-		}
-		if letter == "j" || letter == "x" {
-			return 8
-		}
-		if letter == "k" {
-			return 5
-		}
-		if letter == "f" || letter == "h" || letter == "v" || letter == "w" || letter == "y" {
-			return 4
-		}
-		if letter == "b" || letter == "c" || letter == "m" || letter == "p" {
-			return 3
-		}
-		if letter == "d" || letter == "g" {
-		return 2	
-		}
-		return 1
+func defineValue(letter string) int {
+	if letter == "q" || letter == "z" {
+		return 10
+	}
+	if letter == "j" || letter == "x" {
+		return 8
+	}
+	if letter == "k" {
+		return 5
+	}
+	if letter == "f" || letter == "h" || letter == "v" || letter == "w" || letter == "y" {
+		return 4
+	}
+	if letter == "b" || letter == "c" || letter == "m" || letter == "p" {
+		return 3
+	}
+	if letter == "d" || letter == "g" {
+		return 2
+	}
+	return 1
 }
--- scrabble-score/scrabble_score_test.go
+++ scrabble-score/scrabble_score_test.go
@@ -17,4 +17,3 @@ func BenchmarkScore(b *testing.B) {
 		}
 	}
 }
-
