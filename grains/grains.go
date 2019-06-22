7d38c70f651e022be5d1432dba306cbde17232a5
Renato Suero <suerorenato@gmail.com>
2019-06-22 19:20:57 +0200

    Changed error on grains solution

--- grains/grains.go
+++ grains/grains.go
@@ -1,13 +1,13 @@
 package grains
 
-import "errors"
+import "fmt"
 
 //Square how many grains where on each square
 func Square(value int) (uint64, error) {
 	if value < 1 || value > 64 {
-		return 0, errors.New("Value needs to be	between 1 and 64")
+		return 0, fmt.Errorf("Value needs to be between 1 and 64, the value received was %d", value)
 	}
-	return (1 << uint(value-1)), nil
+	return 1 << uint(value-1), nil
 }
 
 //Total returns total number of grains
